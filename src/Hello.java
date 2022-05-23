b8076a0c9257a7c34f473bb48ca148f5487cb3a2
Md Aman <72564984+md-aman-git@users.noreply.github.com>
2022-05-23 21:03:15 +0530

    added congrats message in Hello if

--- src/Hello.java
+++ src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
         System.out.println("Please enter age : ");
         int age = sc.nextInt();
         if(age >= 18) {
-            System.out.println("You are eligible to vote :)");
+            System.out.println("Congrats! You are eligible to vote :)");
         }
         else {
             throw new AgeException("You are not eligible to vote :(");
